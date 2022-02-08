4066d73eafaaea5a15a22da01aaa7dd6eb225394
Adam Vigaš <92305011+AdamVigas@users.noreply.github.com>
2022-02-08 17:10:05 +0100

    making code better

--- src/battleship/Destroyer.java
+++ src/battleship/Destroyer.java
@@ -3,84 +3,9 @@ package battleship;
 public class Destroyer extends ShipsAbstract{
     private String name  = "Destroyer";
     private int size = 2;
-    private int[] coord = new int[size*2];
 
     public Destroyer() {
-        super.size = this.size;
-    }
-
-
-    @Override
-    public int checkWhatHappend(int [] arr) {
-        for (int i = 0; i < coord.length; i += 2) {
-            //hit target
-            if(coord[i] == arr[0] && coord[i+1] == arr[1]) {
-                size--;
-                if(size == 0) {
-                    return 3;
-                }else return 2;
-            }
-        }
-        return 1;
-    }
-
-    @Override
-    public void Coordinates(int [] arr) {
-        int counter = 0;
-        if(arr[0] == arr[2]) {
-            if(arr[1] < arr[3]) {
-                int num = arr[1];
-                while(num <= arr[3]){
-
-                    coord[counter++] = arr[0];
-                    coord[counter++] = num;
-                    num++;
-                }
-            }else {
-                int num = arr[1];
-                while (num >= arr[3]) {
-                    coord[counter++] = arr[0];
-                    coord[counter++] = num;
-                    num--;
-                }
-            }
-        }else {
-            if(arr[0] < arr[2]) {
-                int num = arr[0];
-                while(num <= arr[2]){
-                    coord[counter++] = num;
-                    coord[counter++] = arr[1];
-                    num++;
-                }
-            }else {
-                int num = arr[0];
-                while (num >= arr[2]) {
-                    coord[counter++] = num;
-                    coord[counter++] = arr[1];
-                    num--;
-                }
-            }
-        }
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-
-    @Override
-    public void setCoord(int[] coord) {
-        this.coord = coord;
-    }
-
-    @Override
-    public int[] getCoord() {
-        return coord;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
+        super.setName(this.name);
+        super.setSize(this.size);
     }
 }
--- src/battleship/ShipsAbstract.java
+++ src/battleship/ShipsAbstract.java
@@ -4,19 +4,82 @@ public abstract class ShipsAbstract {
     /**
      * Field name contains name of ship and size contains size of ship.
      */
-    protected int size;
-    protected String name;
-    protected int[] coord;
+    private int size;
+    private String name;
+    private int[] coord = new int[size*2];
 
+    public int getSize() {
+        return size;
+    }
 
-    public abstract void Coordinates(int [] arr);
+    public String getName() {
+        return name;
+    }
 
-    public abstract int getSize();
-    public abstract String getName();
+    public int[] getCoord() {
+        return coord;
+    }
 
-    public abstract int checkWhatHappend(int [] arr);
+    public void setSize(int size) {
+        this.size = size;
+    }
 
-    public abstract int[] getCoord();
-    public abstract void setCoord(int[] coord);
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCoord(int[] coord) {
+        this.coord = coord;
+    }
+
+    public void Coordinates(int [] arr) {
+        int counter = 0;
+        if(arr[0] == arr[2]) {
+            if(arr[1] < arr[3]) {
+                int num = arr[1];
+                while(num <= arr[3]){
+                    coord[counter++] = arr[0];
+                    coord[counter++] = num;
+                    num++;
+                }
+            }else {
+                int num = arr[1];
+                while (num >= arr[3]) {
+                    coord[counter++] = arr[0];
+                    coord[counter++] = num;
+                    num--;
+                }
+            }
+        }else {
+            if(arr[0] < arr[2]) {
+                int num = arr[0];
+                while(num <= arr[2]){
+                    coord[counter++] = num;
+                    coord[counter++] = arr[1];
+                    num++;
+                }
+            }else {
+                int num = arr[0];
+                while (num >= arr[2]) {
+                    coord[counter++] = num;
+                    coord[counter++] = arr[1];
+                    num--;
+                }
+            }
+        }
+    }
+
+    public int checkWhatHappend(int [] arr) {
+        for (int i = 0; i < coord.length; i += 2) {
+            //hit target
+            if(coord[i] == arr[0] && coord[i+1] == arr[1]) {
+                size--;
+                if(size == 0) {
+                    return 3;
+                }else return 2;
+            }
+        }
+        return 1;
+    }
 
 }
